e66f43e5b1031ad5151b10867a54a49664597f5f
Yunnna005 <annakovalenko0500@gmail.com>
2025-12-03 16:23:37 +0000

    Add color to Dashboard

--- app/ExpensesTracker/src/componenets/NavBar.tsx
+++ app/ExpensesTracker/src/componenets/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
-    return <ul className="menu bg-base-200 rounded-box w-auto h-screen fixed top-0 left-0 p-4 gap-4">
-        <p className="text-2xl font-bold p-4">Expenses Tracker</p>
-        <li className="text-lg"><a>Dashboard</a></li>
-        <li className="text-lg"><a>Chat</a></li>
+    return <ul className="menu bg-sky-950 w-auto h-screen fixed top-0 left-0 p-4 gap-5">
+        <p className="text-2xl font-bold p-3 pr-7 mb-4 text-slate-200">Expenses Tracker</p>
+        <li className="text-lg font-bold text-sky-100 hover:bg-sky-900"><a>Dashboard</a></li>
+        <li className="text-lg font-bold text-sky-700 menu-disabled"><a>Chat</a></li>
     </ul>;
 }
--- app/ExpensesTracker/src/templates/Template.tsx
+++ app/ExpensesTracker/src/templates/Template.tsx
@@ -3,7 +3,7 @@ import NavBar from "../componenets/NavBar";
 export default function Template({ children }: { children: React.ReactNode }) {
     return <div>
     <NavBar />  
-        <div className="ml-64">
+        <div className="ml-63">
             {children}
         </div>  
     </div>;
